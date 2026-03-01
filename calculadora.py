60299168fdecdd5b481e6b56fb305a8d67e5bb1f
Breebreecode <brendafrancaesouza@gmail.com>
2026-03-01 09:28:36 -0400

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    return a + b
+
+print("=== CALCULADORA ===")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("Resultado:", soma(num1, num2))
